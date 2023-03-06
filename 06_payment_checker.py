a3661b110a47fdd51654ea26bbcff367ea612b60
k0881 <91438592+k0881@users.noreply.github.com>
2023-03-07 09:13:19 +1300

    Add files via upload

---
+++ 06_payment_checker.py
@@ -0,0 +1,20 @@
+# functions go here
+def cash_credit(question):
+
+    while True:
+        response = input(question).lower()
+
+        if response == "cash" or response == "ca":
+            return "cash"
+
+        elif response == "credit" or response == "cr":
+            return "credit"
+
+        else:
+            print("Please choose a valid payment method")
+
+
+# main routine goes here
+while True:
+    payment_method = cash_credit("Choose a payment method (Cash"
+                                 "or credit): ")
